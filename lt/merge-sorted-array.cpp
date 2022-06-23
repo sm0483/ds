65d57e28aa3ec822c30ae237842deffaf9689841
Sreerag <msratofficial@gmail.com>
2022-06-23 20:26:12 +0530

    Solution not optimised

---
+++ lt/merge-sorted-array.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=0,j=0;
+        vector<int>answer;
+        while(i <m && j < n){
+            if(nums1[i]>nums2[j]){
+                answer.push_back(nums2[j]);
+                j++;
+            }
+            else if(nums1[i]<nums2[j]){
+                answer.push_back(nums1[i]);
+                i++;
+
+            }
+            else{
+                answer.push_back(nums1[i]);
+                i++;
+            }
+        }
+        nums1={};
+
+        for(auto i:answer){
+            nums1.push_back(i);
+        }
+
+
+        
+    }
+};
+
+
+int main(){
+    merge();
+
+    return 0;
+
+
+}
